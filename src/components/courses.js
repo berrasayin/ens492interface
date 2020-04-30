2c19a4b6b26dd6d606465e59aa6a41827fbbd3db
dsengul <dsengul@sabanciuniv.edu>
2020-04-30 06:29:05 +0300

    asking questions and getting answers

--- src/components/courses.js
+++ src/components/courses.js
@@ -16,156 +16,187 @@ import DialogMicro from './DialogMicro';
 
 class Courses extends React.Component {
 
-    constructor() {
-      super();
-      this.state = {
-        activeStep: 0,
-        steps: [],
-        videos: [],
-        try: [],
-        modalShow: true,
-      };
-    }
-  
-    componentDidMount = async () => {
-  
-      firebase.database().ref("microTopics").child("courses").once("value").then(data => {
-        let videosDict = data.val()
-        for (let key in videosDict) {
-          //console.log( videosDict[key])
-          this.setState({
-            videos: [...this.state.videos, videosDict[key]],
-            steps: [...this.state.steps, key]
-          })
-        }
-        console.log(this.state.videos);
-        console.log(this.state.steps);
-      });
-      // await fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json()).then((result) => {
-      //   this.setState({
-      //     try: result
-      //   })
-      //   console.log(this.state.try)
-      // })
-  
-  
-      // await fetch('https://jsonplaceholder.typicode.com/users', {
-      //   method: 'POST',
-      //   body: JSON.stringify({
-      //     id: 11,
-      //     name: 'Jane Doe',
-      //     username: 'janedoe',
-      //     email: 'janedoe@biz',
-      //   }),
-      //   headers: {
-      //     "Content-type": "application/json; charset=UTF-8"
-      //   }
-      // }).then(response => response.json())
-      //   .then(json => console.log(json))
-  
-    }
-  
-    setModalShow = (state) => {
-      this.setState({
-        modalShow: state,
-      });
+  constructor() {
+    super();
+    this.state = {
+      activeStep: 0,
+      steps: [],
+      videos: [],
+      try: [],
+      modalShow: true,
+      answerKey: [],
+      questions: [],
+      answer: '',
     };
-  
-  
-    getStepContent = (step) => {
-      switch (step) {
-        case 0:
-          return <ReactPlayer
-            className='react-player'
-            url={this.state.videos[0]}
-            width='700px'
-            height='500px'
-            controls={true} />
-        case 1:
-          return <ReactPlayer
-            className='react-player'
-            url={this.state.videos[1]}
-            width='700px'
-            height='500px'
-            controls={true} />
-        case 2:
-          return <ReactPlayer
-            className='react-player'
-            url={this.state.videos[2]}
-            width='700px'
-            height='500px'
-            controls={true} />
-        default:
-          return 'Unknown step';
-      }
-    }
-  
-  
-    handleNext = async () => {
-      await this.setState(
-        prevState => ({ activeStep: prevState.activeStep + 1 }
-        ));
-    }
-  
-    handleBack = async () => {
-      await this.setState(
-        prevState => ({ activeStep: prevState.activeStep - 1 }
-        ));
-    }
-  
-    handleReset = () => {
-      this.setState({
-        activeStep: 0
-      })
-    };
+  }
 
-    render() {
-  
-      return (
-        <div className='main' >
-          <DialogMicro
-            show={this.state.modalShow}
-          onHide={() => this.setModalShow(false)}/>
-          <Stepper className='micro-topics' activeStep={this.state.activeStep} orientation="vertical">
-            {localStorage !== null &&
-              this.state.steps.slice(0, JSON.parse(localStorage.getItem('micronum'))).map((label, index) => (
-                < Step  key={label} >
-                  <StepLabel >{label}</StepLabel>
-                  <StepContent>
-                    <Typography >{this.getStepContent(index)}</Typography>
-                    <div className='actionsContainer'>
-                      <div>
-                        <Button
-                          disabled={this.state.activeStep === 0 ? true : false}
-                          onClick={this.handleBack}
-                          className='click'
-                        >
-                          Back
-                    </Button>
-                        <Button
-                          variant="contained"
-                          color="primary"
-                          onClick={this.handleNext}
-                          className='click'
-                        >
-                          {this.state.activeStep === JSON.parse(localStorage.getItem('micronum')) - 1 ? 'Finish' : 'Next'}
-                        </Button>
-                      </div>
-                    </div>
-                  </StepContent>
-                </Step>
-              ))}
-          </Stepper>
-          {this.state.activeStep === this.state.steps.slice(0, JSON.parse(localStorage.getItem('micronum'))).length && (
-            <Paper square elevation={0} className='resetContainer'>
-              <Typography>All micro topics are completed - you&apos;re finished</Typography>
-              <Button onClick={this.handleReset} className='click'>
-                Reset
-            </Button>
-            </Paper>
-          )}
-        </div >
-      );
+  componentDidMount = async () => {
+
+    firebase.database().ref("microTopics").child("courses").once("value").then(data => {
+      let videosDict = data.val()
+      for (let key in videosDict) {
+        //console.log( videosDict[key])
+        this.setState({
+          videos: [...this.state.videos, videosDict[key]],
+          steps: [...this.state.steps, key]
+        })
+      }
+    });
+    firebase.database().ref("microTopics").child("quizs").once("value").then(data => {
+      let quizes = data.val()
+      console.log(quizes)
+      for (let key in quizes) {
+        let quiz = quizes[key]
+        this.setState({
+          answerKey: [...this.state.answerKey, quiz["answer"]],
+          questions: [...this.state.questions, quiz["question"]]
+        })
+      }
+    });
+    // await fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json()).then((result) => {
+    //   this.setState({
+    //     try: result
+    //   })
+    //   console.log(this.state.try)
+    // })
+
+
+    // await fetch('https://jsonplaceholder.typicode.com/users', {
+    //   method: 'POST',
+    //   body: JSON.stringify({
+    //     id: 11,
+    //     name: 'Jane Doe',
+    //     username: 'janedoe',
+    //     email: 'janedoe@biz',
+    //   }),
+    //   headers: {
+    //     "Content-type": "application/json; charset=UTF-8"
+    //   }
+    // }).then(response => response.json())
+    //   .then(json => console.log(json))
+
+  }
+
+  setModalShow = (state) => {
+    this.setState({
+      modalShow: state,
+    });
+  };
+
+
+  getStepContent = (step) => {
+    switch (step) {
+      case 0:
+        return <ReactPlayer
+          className='react-player'
+          url={this.state.videos[0]}
+          width='700px'
+          height='500px'
+          controls={true} />
+      case 1:
+        return <ReactPlayer
+          className='react-player'
+          url={this.state.videos[1]}
+          width='700px'
+          height='500px'
+          controls={true} />
+      case 2:
+        return <ReactPlayer
+          className='react-player'
+          url={this.state.videos[2]}
+          width='700px'
+          height='500px'
+          controls={true} />
+      default:
+        return 'Unknown step';
     }
   }
-  export default Courses;
+
+
+  handleNext = async () => {
+    await this.setState(
+      prevState => ({ activeStep: prevState.activeStep + 1 }
+      ));
+  }
+
+  handleBack = async () => {
+    await this.setState(
+      prevState => ({ activeStep: prevState.activeStep - 1 }
+      ));
+  }
+
+  handleReset = () => {
+    this.setState({
+      activeStep: 0
+    })
+  };
+
+  saveAnswer = (event) => {
+    localStorage.setItem('answer', this.state.answer);
+    console.log(this.state.answer);
+    event.target.value = '';
+  }
+  handleAnswer = (event) => {
+    this.setState({
+      answer: [...this.state.answer, event.target.value],
+    });
+  }
+
+  render() {
+
+    return (
+      <div className='main' >
+        <DialogMicro
+          show={this.state.modalShow}
+          onHide={() => this.setModalShow(false)} />
+        <Stepper className='micro-topics' activeStep={this.state.activeStep} orientation="vertical">
+          {localStorage !== null &&
+            this.state.steps.slice(0, JSON.parse(localStorage.getItem('micronum'))).map((label, index) => (
+              < Step key={label} >
+                <StepLabel >{label}</StepLabel>
+                <StepContent>
+                  <Typography >{this.getStepContent(index)}</Typography>
+                  <div className='actionsContainer'>
+                    <div>
+                      <img src={this.state.questions[index]} />
+                      {/* <Button
+                        disabled={this.state.activeStep === 0 ? true : false}
+                        onClick={this.handleBack}
+                        className='click'
+                      >
+                        Back
+                    </Button> */}
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={this.handleNext}
+                        className='click'
+                      >
+                        {this.state.activeStep === JSON.parse(localStorage.getItem('micronum')) - 1 ? 'Finish' : 'Next'}
+                      </Button>
+                      <form>
+                        <label>
+                          Answer:
+                          <input type="text" name="name" placeholder="Enter A, B and so on" onChange={this.handleAnswer} />
+                          <Button onClick={this.saveAnswer}>Save your answer</Button>
+                        </label>
+                      </form>
+                    </div>
+                  </div>
+                </StepContent>
+              </Step>
+            ))}
+        </Stepper>
+        {this.state.activeStep === this.state.steps.slice(0, JSON.parse(localStorage.getItem('micronum'))).length && (
+          <Paper square elevation={0} className='resetContainer'>
+            <Typography>All micro topics are completed - you&apos;re finished</Typography>
+            <Button onClick={this.handleReset} className='click'>
+              Reset
+            </Button>
+          </Paper>
+        )}
+      </div >
+    );
+  }
+}
+export default Courses;
